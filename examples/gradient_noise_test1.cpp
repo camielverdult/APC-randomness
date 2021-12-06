58840fb3db0967efc865d8fa7c1aa11c84933851
marnixlaar <74597221+marnixlaar@users.noreply.github.com>
2021-12-06 14:36:45 +0100

    update gradient noise test

--- examples/gradient_noise_test1.cpp
+++ examples/gradient_noise_test1.cpp
@@ -5,73 +5,175 @@
 #include <cmath>
 #include <cstdio>
 #include <random>
+#include <iostream>
 
-//modulus m: 2^31 - 1
-const unsigned long m = 2147483647;
+std::linear_congruential_engine<unsigned long, 1103515245, 12345, 2147483647> lcg;
 
-//multiplier a: 1103515245
-const unsigned long a = 1103515245;
-
-// increment c: 74
-const unsigned long c = 12345;
-
-
-std::linear_congruential_engine<unsigned long, a, c, m> lcg;
-
-
-unsigned int hash(unsigned int a) {
-//    a = (a ^ 61) ^ (a >> 16);
-//    a = a + (a << 3);
-//    a = a ^ (a >> 4);
-//    a = a * 0x27d4eb2d;
-//    a = a ^ (a >> 15);
-//    srand(a);
-    lcg.seed(a);
-    return lcg();
-}
-
-unsigned int ivalue(int x, int y) {
-    return hash(y<<16|x)&0xff;
-}
+std::subtract_with_carry_engine<unsigned long, 32, 128, 159> swc;
 
 float smooth(float x) {
     return 6*x*x*x*x*x - 15*x*x*x*x + 10*x*x*x;
 }
 
-float value(float x, float y) {
+///-------------------------------------------------------------------------------------
+
+unsigned int lcg_hash(unsigned int a) {
+    lcg.seed(a);
+    return lcg();
+}
+
+unsigned int lcg_ivalue(int x, int y) {
+    return lcg_hash(y<<16|x)&0xff;
+}
+
+float lcg_value(float x, float y) {
     int ix = floor(x);
     int iy = floor(y);
     float fx = smooth(x-ix);
     float fy = smooth(y-iy);
 
-    int v00 = ivalue(iy+0, ix+0);
-    int v01 = ivalue(iy+0, ix+1);
-    int v10 = ivalue(iy+1, ix+0);
-    int v11 = ivalue(iy+1, ix+1);
+    int v00 = lcg_ivalue(iy+0, ix+0);
+    int v01 = lcg_ivalue(iy+0, ix+1);
+    int v10 = lcg_ivalue(iy+1, ix+0);
+    int v11 = lcg_ivalue(iy+1, ix+1);
     float v0 = v00*(1-fx) + v01*fx;
     float v1 = v10*(1-fx) + v11*fx;
     return v0*(1-fy) + v1*fy;
 }
 
+///-------------------------------------------------------------------------------------
+
+unsigned int swc_hash(unsigned int a) {
+    swc.seed(a);
+    return swc();
+}
+
+unsigned int swc_ivalue(int x, int y) {
+    return swc_hash(y<<16|x)&0xff;
+}
+
+float swc_value(float x, float y) {
+    int ix = floor(x);
+    int iy = floor(y);
+    float fx = smooth(x-ix);
+    float fy = smooth(y-iy);
+
+    int v00 = swc_ivalue(iy+0, ix+0);
+    int v01 = swc_ivalue(iy+0, ix+1);
+    int v10 = swc_ivalue(iy+1, ix+0);
+    int v11 = swc_ivalue(iy+1, ix+1);
+    float v0 = v00*(1-fx) + v01*fx;
+    float v1 = v10*(1-fx) + v11*fx;
+    return v0*(1-fy) + v1*fy;
+}
+
+///-------------------------------------------------------------------------------------
+
+unsigned int std_rand_hash(unsigned int a) {
+    srand(a);
+    return rand();
+}
+
+unsigned int std_rand_ivalue(int x, int y) {
+    return std_rand_hash(y<<16|x)&0xff;
+}
+
+float std_rand_value(float x, float y) {
+    int ix = floor(x);
+    int iy = floor(y);
+    float fx = smooth(x-ix);
+    float fy = smooth(y-iy);
+
+    int v00 = std_rand_ivalue(iy+0, ix+0);
+    int v01 = std_rand_ivalue(iy+0, ix+1);
+    int v10 = std_rand_ivalue(iy+1, ix+0);
+    int v11 = std_rand_ivalue(iy+1, ix+1);
+    float v0 = v00*(1-fx) + v01*fx;
+    float v1 = v10*(1-fx) + v11*fx;
+    return v0*(1-fy) + v1*fy;
+}
+
+///-------------------------------------------------------------------------------------
+
+
 unsigned char pic[1024*1024];
 
 int main() {
-    for (int y=0; y<1024; y++) {
-        for (int x=0; x<1024; x++) {
-            float v = 0;
 
-            for (int o=0; o<=9; o++) {
-                v += value(x/64.0f*(1<<o), y/64.0f*(1<<o))/(1<<o);
+    int choice{};
+    std::string engine{};
+
+    std::cout << "Please select the engine to use\n"
+                 "\t1: Linear congruential engine\n"
+                 "\t2: Subtract with carry engine\n"
+                 "\t3: Std::rand() (C++11 rand)" << std::endl;
+
+    std::cin >> choice;
+
+    switch (choice){
+        case 1:
+            std::cout << "Using LCG engine" << std::endl;
+            for (int y=0; y<1024; y++) {
+                for (int x=0; x<1024; x++) {
+                    float v = 0;
+
+                    for (int o=0; o<=9; o++) {
+                        v += lcg_value(x/64.0f*(1<<o), y/64.0f*(1<<o))/(1<<o);
+                    }
+
+                    int r = rint(v*0.5f);
+
+                    pic[y*1024+x] = r;
+                }
             }
+            engine = "lcg";
+            break;
+        case 2:
+            std::cout << "Using SWC engine" << std::endl;
+            for (int y=0; y<1024; y++) {
+                for (int x=0; x<1024; x++) {
+                    float v = 0;
 
-            int r = rint(v*0.5f);
+                    for (int o=0; o<=9; o++) {
+                        v += swc_value(x/64.0f*(1<<o), y/64.0f*(1<<o))/(1<<o);
+                    }
 
-            pic[y*1024+x] = r;
-        }
+                    int r = rint(v*0.5f);
+
+                    pic[y*1024+x] = r;
+                }
+            }
+            engine = "swc";
+            break;
+        case 3:
+            std::cout << "Using C++11 std::rand()" << std::endl;
+            for (int y=0; y<1024; y++) {
+                for (int x=0; x<1024; x++) {
+                    float v = 0;
+
+                    for (int o=0; o<=9; o++) {
+                        v += std_rand_value(x/64.0f*(1<<o), y/64.0f*(1<<o))/(1<<o);
+                    }
+
+                    int r = rint(v*0.5f);
+
+                    pic[y*1024+x] = r;
+                }
+            }
+            engine = "std_rand";
+            break;
+        default:
+            std::cout << "Invalid choice" << std::endl;
+            break;
+
     }
 
-    FILE *f = fopen("gradient_noise_lcg.pnm", "wb");
+    std::string filename = "gradient_noise_" + engine + ".pnm";
+
+    FILE *f = fopen(filename.c_str(), "wb");
     fprintf(f, "P5\n1024 1024\n255\n");
     fwrite(pic, 1, 1024*1024, f);
     fclose(f);
+
+    std::cout << "Image saved to: " << filename << std::endl;
 }
